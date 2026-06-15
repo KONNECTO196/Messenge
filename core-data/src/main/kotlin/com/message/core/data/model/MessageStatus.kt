8d8d617f2c6e64f0a48245466397ef7626802bff
KONNECTO196 <rd0937923@gmail.com>
2026-06-15 08:57:44 +0530

    Create core-data/src/main/kotlin/com/message/core/data/model/MessageStatus.kt

---
+++ core-data/src/main/kotlin/com/message/core/data/model/MessageStatus.kt
@@ -0,0 +1,28 @@
+/*
+ * MIT License
+ * Copyright (c) 2024 Message App
+ * See LICENSE file in root directory for full license text.
+ */
+
+package com.message.core.data.model
+
+enum class MessageStatus {
+    None, Sending, Sent, Delivered, Read, Failed;
+
+    companion object {
+        fun from(status: Int, boxType: Int): MessageStatus = when {
+            boxType == 5    -> Failed
+            boxType == 4    -> Sending
+            status == 0     -> Delivered
+            status == 32    -> Sent
+            status == 64    -> Failed
+            else            -> Sent
+        }
+    }
+}
+
+enum class MessageType { SMS, MMS, RCS }
+
+enum class MessageProtocol { SMS, MMS, RCS }
+
+enum class ConversationSortOrder { ByDate, ByName, UnreadFirst }
